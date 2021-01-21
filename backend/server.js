64a5c86d26aca6add3fd7fbabac725388d10af39
Ademayowa adeniyi <ademayowaadeniyi@gmail.com>
2021-01-21 20:31:06 +0100

    added heroku scripts

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import colors from 'colors';
-import mongoSanitize from 'express-mongo-sanitize';
-import { errorHandler } from './middleware/errorMiddleware.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import jobs from './routes/jobs.js';
 
@@ -12,17 +11,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const API_PREFIX = '/api/v1';
-
 app.use(express.json());
 
-// Dev logging middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-// Sanitize inputs
-app.use(mongoSanitize());
+// Mount routers
+const API_PREFIX = '/api/v1';
+app.use(`${API_PREFIX}/jobs`, jobs);
 
 const __dirname = path.resolve();
 // Serve static assets if in production
@@ -34,20 +27,13 @@ if (process.env.NODE_ENV === 'production') {
   );
 }
 
-// Mount routers
-app.use(`${API_PREFIX}/jobs`, jobs);
+app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
   )
 );
-
-// Handle unhandled promise rejection
-process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
-});
